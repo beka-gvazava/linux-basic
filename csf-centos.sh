bd6da9756c9656c8d8896460e3fd283d8efb9070
beka-gvazava <125278101+beka-gvazava@users.noreply.github.com>
2023-04-21 20:19:48 +0400

    Update csf-centos.sh

--- csf-centos.sh
+++ csf-centos.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-yum install wget zip unzip;wget wget https://download.configserver.com/csf.tgz;tar -xzf csf.tgz;cd csf;./install.cpanel.sh
+yum install wget zip unzip
+wget wget https://download.configserver.com/csf.tgz
+tar -xzf csf.tgz
+cd csf
+;bash install.cpanel.sh
